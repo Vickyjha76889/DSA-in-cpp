596e75e72f1bedf2b83f7c7d7823d235bc5c9ef9
vicky <try.vickyjha@gmail.com>
2025-08-14 19:50:52 +0530

    zigzag traversal

---
+++ lecture64/zigzagTraverdsal.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+
+void buildTree(node* &root){
+    queue<node*> q;
+    cout<<"enter the data ";
+    int data;
+    cin>>data;
+    root = new node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+
+        cout<<"Enter the data for left"<<temp->data<<endl;
+        int leftdata;
+        cin>>leftdata;
+
+        if(leftdata != -1){
+            temp->left = new node(leftdata);
+            q.push(temp->left);
+        }
+
+        cout<<"Enter the data for right"<<temp->data<<endl;
+        int rightdata;
+        cin>>rightdata;
+
+        if(rightdata != -1){
+            temp->right = new node(rightdata);
+            q.push(temp->right);
+        }
+    }
+}
+
+vector<int> zigzagTraversal(node* root){
+    vector<int> result;
+    if(root == NULL)
+       return result;
+     
+    queue<node*> q;
+    q.push(root);
+    bool lefttoright = true;
+     
+    while(!q.empty()){
+        int size = q.size();
+        vector<int> ans(size);
+        for(int i = 0; i<size ; ++i){
+            node* frontend = q.front();
+            q.pop();
+
+            int index = lefttoright? i: size-i-1;
+            ans[index] = frontend->data;
+
+            if(frontend->left){
+                q.push(frontend->left);
+            }
+            if(frontend->right){
+                q.push(frontend->right);
+            }            
+        }
+
+        lefttoright = !lefttoright;
+
+        for(int val: ans){
+                result.push_back(val);
+                cout<<val<<" ";
+        }
+            cout<<endl;
+    }    
+    return result;
+}
+
+int main(){
+    node* root = NULL;
+    buildTree(root);
+    zigzagTraversal(root);
+
+}
