f3b6649b53c12a84819d272f9ff0836d256ff9a8
vicky <try.vickyjha@gmail.com>
2025-10-21 11:48:33 +0530

    smallest Range in k list

---
+++ lecture77/1SmallestRangeInKList.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<climits>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    int row;
+    int col;
+
+    node(int data, int row , int col){
+        this->data = data;
+        this->row = row;
+        this->col = col;
+    }
+};
+
+class compare{
+    public:
+    bool operator()(node* a,node* b){
+        return a->data > b->data;
+    }
+};
+
+int ksorted(vector<vector<int>> &a , int k , int n){
+    priority_queue<node*,vector<node*>,compare> minheap;
+    int maxi = INT_MIN;
+    int mini = INT_MAX;
+    
+    for(int i = 0 ; i<k ;i++){
+        int element = a[i][0];
+        maxi = max(maxi,element);
+        mini = min(mini,element);
+        minheap.push(new node(element,i,0));
+    }
+    int start = mini ;
+    int end = maxi;
+
+    while(true){
+        node* temp = minheap.top();
+        minheap.pop();
+        mini = temp->data;
+
+        int row = temp->row;
+        int col = temp->col;
+
+        if((end - start) >(maxi - mini)){
+            start = mini;
+            end = maxi;
+        }
+
+        if(col+1 > n){
+            break;
+        }
+        
+        int element = a[row][col + 1];
+        maxi = max(maxi,element);
+        minheap.push(new node(element,row,col + 1));
+        delete temp;
+
+    }
+    cout<<"Range start: "<<start<<" end: "<<end<<endl;
+    return end - start;
+    
+
+
+}
+
+int main(){
+  vector<vector<int>> a = {
+    {1,5,7},
+    {3,6,9},
+    {2,4,8},
+  };
+  int k = a.size();
+  int n = a[0].size();
+  cout<<ksorted(a,k,n);
+  return 0;
+    
+}
+
