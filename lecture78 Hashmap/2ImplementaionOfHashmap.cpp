3755fbe54044ccee310a4d3f336f73b24ef64be0
vicky <try.vickyjha@gmail.com>
2025-10-27 21:14:55 +0530

    basic of hashmap

--- lecture78 Hashmap/2ImplementaionOfHashmap.cpp
+++ lecture78 Hashmap/2ImplementaionOfHashmap.cpp
@@ -8,14 +8,41 @@
 using namespace std;
 
 int main(){
-    unordered>map<string,int> map;
+    unordered_map<string,int> m;
     // insetion 
     pair<string,int> p = make_pair("vicky",1);
-    map.insert(p);
+    m.insert(p);
     
     // another method of insertion
-    map["vicky"] = 3;
+    m["vicky"] = 3;
     // this will update the value of vicky to 3 from 1 instead of creating of making another 
+    m["mrinal"] = 2;
+    m["aman"] = 6;
+    m["prem"] = 4;
+    m["ani"] = 5;
+    // searching
+    cout<<m["vicky"]<<endl;
+    //cout<<m.at("unknown")<<endl; // this will give error if key is not present
+
+    cout<<m["unknown"]<<endl; // but this will not give error it will insert the key with value 0;
+    cout<<m.at("unknown")<<endl;
+
+    cout<<m.size()<<endl;
 
     
+
+    cout<<m.count("mrinal")<<endl;// this will return 0 if key is not present else it will return 1;
+    cout<<m.count("vicky")<<endl;
+
+    cout<<m.count("unknown")<<endl;
+    //deletion
+    m.erase("unknown");
+    cout<<m.count("unknown")<<endl;
+
+    // iteration
+
+    for(auto i:m){
+        cout<<i.first<< " " <<i.second <<endl;
+    }
+
 }
